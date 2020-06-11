a64ef3d048c5d8cb0ed9dfc485cf368dae9605f1
Federico Soich <federico@andromedalatam.com>
2020-06-11 09:24:58 -0300

    mp

--- success.php
+++ success.php
@@ -3,8 +3,8 @@
   
  MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
  MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-
-           $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+ echo 'GRACIAS POR TU COMPRA !!';
+           $payment = new MercadoPago\Payment().find_by_id($_POST["id"]);
            print_r($payment);
            return 200;
 
