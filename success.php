3606817210744b33e6d7da4be1171bb5519d3f57
Federico Soich <federico@andromedalatam.com>
2020-06-11 12:09:01 -0300

    mp

--- success.php
+++ success.php
@@ -1,11 +1,18 @@
 <?php
- require_once 'vendor/autoload.php';
-  
- MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
- MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://api.mercadopago.com/v1/payments/'.$_POST['collection_id'].'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+
  echo 'GRACIAS POR TU COMPRA !!';
-           $payment = new MercadoPago\Payment().find_by_id($_POST["id"]);
-           print_r($payment);
+           print_r($result);
            return 200;
 
 ?>
